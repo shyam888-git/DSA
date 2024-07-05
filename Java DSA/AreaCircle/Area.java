b52a5901b60d402a1d04e526e1bd9332daf7acd3
Shyam Sharki <ssk66221@gmail.com>
2024-07-05 05:57:52 +0545

    area

--- Java DSA/AreaCircle/Area.java
+++ Java DSA/AreaCircle/Area.java
@@ -1,1 +1,11 @@
- git push --set-upstream origin dev
+public class CricleArea{
+    public static void PrintArea(double radius){
+        double result=Math.PI*radius*radius;
+        System.out.println("Area of circle is "+""+result);
+    }
+    public static void main(String[]args){
+        double r=3.25;
+        PrintArea(r);
+        
+    }
+}
